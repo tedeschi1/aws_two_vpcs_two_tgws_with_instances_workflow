dd9ecc4133ceb6dd8d964aec7e4ff888ed622e50
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-16 16:48:00 -0600

    Create aws_tgw_east2.tf

---
+++ aws-vpcs-instances-tgws/aws_tgw_east2.tf
@@ -0,0 +1,35 @@
+resource "aws_ec2_transit_gateway" "us_east2_hub" {
+  description = "US East2 Hub for VPC peering"
+  provider = aws.east2
+  
+  auto_accept_shared_attachments = "enable"
+  default_route_table_association = "enable"
+  default_route_table_propagation = "enable"
+
+  tags = {
+    Name = "us-east2-hub-tgw"
+  }
+}
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "vpc1_east2" {
+  subnet_ids         = [aws_subnet.vpc1_east2_subnets[0].id, aws_subnet.vpc1_east2_subnets[1].id]
+  transit_gateway_id = aws_ec2_transit_gateway.us_east2_hub.id
+  vpc_id             = aws_vpc.vpc1_east2.id
+  provider = aws.east2
+
+  tags = {
+    Name = "vpc1-east2-attachment"
+  }
+}
+
+resource "aws_ec2_transit_gateway_route" "to_vpc1_east1_tgw" {
+  destination_cidr_block         = "10.1.0.0/21" # East-1 CIDR's
+  provider = aws.east2
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.tgw_peering.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway.us_east2_hub.propagation_default_route_table_id
+  depends_on = [ aws_ec2_transit_gateway.us_east2_hub ]
+}
+
+output "aws_ec2_east2_tgw_route_table_id" {
+  value = aws_ec2_transit_gateway.us_east2_hub.propagation_default_route_table_id
+}
