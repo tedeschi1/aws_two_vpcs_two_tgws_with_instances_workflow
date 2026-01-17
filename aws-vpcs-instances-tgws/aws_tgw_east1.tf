12f98e80441fdebb14c531e516164e52004000a6
tedeschi1 <31430344+tedeschi1@users.noreply.github.com>
2026-01-17 16:43:47 -0600

    update route tag

--- aws-vpcs-instances-tgws/aws_tgw_east1.tf
+++ aws-vpcs-instances-tgws/aws_tgw_east1.tf
@@ -27,10 +27,6 @@ resource "aws_ec2_transit_gateway_route" "to_vpc1_east2_tgw" {
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.tgw_peering.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway.us_east1_hub.propagation_default_route_table_id
   depends_on = [ aws_ec2_transit_gateway.us_east1_hub ]
-
-  tags = {
-    Name = "route-to-us-east2"
-  }
 }
 
 output "aws_ec2_tgw_route_table_id" {
